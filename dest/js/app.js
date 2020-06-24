c8833a830aad4dde34976b15749379140207dcab
Andrii <Luckyhidalgo@gmail.com>
2020-06-24 11:35:10 +0300

    Added lazy loading for image and background images.

--- dest/js/app.js
+++ dest/js/app.js
@@ -139,6 +139,64 @@ var initSwiper = function initSwiper() {
 			}, 1000);
 		});
 	};
+
+	var lazyLoadMedia = function lazyLoadMedia() {
+		document.addEventListener("DOMContentLoaded", function () {
+			var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+			var active = false;
+
+			var lazyLoad = function lazyLoad() {
+				if (active === false) {
+					active = true;
+
+					setTimeout(function () {
+						lazyImages.forEach(function (lazyImage) {
+							if (lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0 && getComputedStyle(lazyImage).display !== "none") {
+								lazyImage.src = lazyImage.dataset.src;
+								lazyImage.srcset = lazyImage.dataset.srcset;
+								lazyImage.classList.remove("lazy");
+
+								lazyImages = lazyImages.filter(function (image) {
+									return image !== lazyImage;
+								});
+
+								if (lazyImages.length === 0) {
+									document.removeEventListener("scroll", lazyLoad);
+									window.removeEventListener("resize", lazyLoad);
+									window.removeEventListener("orientationchange", lazyLoad);
+								}
+							}
+						});
+
+						active = false;
+					}, 200);
+				}
+			};
+
+			document.addEventListener("scroll", lazyLoad);
+			window.addEventListener("resize", lazyLoad);
+			window.addEventListener("orientationchange", lazyLoad);
+		});
+
+		document.addEventListener("DOMContentLoaded", function () {
+			var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-background"));
+
+			if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
+				var lazyBackgroundObserver = new IntersectionObserver(function (entries, observer) {
+					entries.forEach(function (entry) {
+						if (entry.isIntersecting) {
+							entry.target.classList.add("visible");
+							lazyBackgroundObserver.unobserve(entry.target);
+						}
+					});
+				});
+
+				lazyBackgrounds.forEach(function (lazyBackground) {
+					lazyBackgroundObserver.observe(lazyBackground);
+				});
+			}
+		});
+	};
 	/*
  * CALLBACK :: end
  * ============================================= */
@@ -163,7 +221,11 @@ var initSwiper = function initSwiper() {
 		inputFocusAnimate();
 		menuToggle();
 		scrollToTop();
+		lazyLoadMedia();
 		// ==========================================
 	};
-	initNative();
+
+	$(window).on('load', function (ev) {
+		initNative();
+	});
 })();
